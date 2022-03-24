fc0622dcf23c4b71eee7926868687925f0ae0e7f
Ifeoluwa O. Onifade <ifeonifade@yahoo.com>
2022-03-24 02:06:34 +0000

    edit to quote script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - print quote
  *
- * Return: Always 1 (Error)
+ * Description: prints Dors's quote
+ *
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, "- Dora Korpar, 2015-10-19\n", 58);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
