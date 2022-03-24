28cb23ec54941c17a9113ddee7fc2b7227196ddb
Ifeoluwa O. Onifade <ifeonifade@yahoo.com>
2022-03-24 00:42:05 +0000

    edit c code in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-/** This code prints exactly "Programming is like building a multilingual puzzle"
- * Written in C
+/**
  * main- Entry point
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 { puts('Programming is like building a multilingual puzzle');
 	return (0);
 }
