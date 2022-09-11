52ebf57c673cfb3980b5de664df3d143be09ec5f
Ifeoluwa <ifeonifade@yahoo.com>
2022-09-11 10:53:38 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* prints all single digit numbers of base 10 */
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i <= 9; i++)
+		printf("%d", i)
+	printf("\n");
+	return (0);
+}
