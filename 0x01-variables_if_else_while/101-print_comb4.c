0cb114e06841fb8f1ffa4ec29d34bb9ade47df55
Ifeoluwa <ifeonifade@yahoo.com>
2022-09-11 12:20:49 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints all possible
+ * combinations of three digits
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = '0';
+	int j;
+	int k;
+
+	while (i <= '9')
+	{
+		j = '0';
+		while (k <= '9')
+		{
+			if (i < j && j < k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i == '7' && j == '8' && k == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+			k++;
+		}
+		j++;
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
