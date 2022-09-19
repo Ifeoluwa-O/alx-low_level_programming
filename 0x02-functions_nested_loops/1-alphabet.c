df9447fad29a671b5687ba83fc2ce649e10f3e02
Ifeoluwa <ifeonifade@yahoo.com>
2022-09-19 13:48:09 -0700

    task 1- alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
