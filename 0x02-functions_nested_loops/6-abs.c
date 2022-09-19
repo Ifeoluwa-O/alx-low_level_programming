3aa464ec0f87c8049edcbe840dcb5e69224d7484
Ifeoluwa <ifeonifade@yahoo.com>
2022-09-19 16:19:10 -0700

    task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - a function that returns absolute value
+ * @int: integer
+ * Return: absolute vale of variable
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
