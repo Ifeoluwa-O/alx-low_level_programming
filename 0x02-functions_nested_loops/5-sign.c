3805ba82c946dcf5ee1478be430e457a4a636052
Ifeoluwa <ifeonifade@yahoo.com>
2022-09-19 15:53:35 -0700

    try

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n : number
- * Return : value 0
+ * Return value : an integer- 0, 1 or -1
  */
 
 int print_sign(int n)
@@ -11,16 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
 	}
+	return (1);
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
 	}
+	return (0);
 	else
 	{
 		_putchar('-');
-		return (-1);
 	}
+	return (-1);
 }
