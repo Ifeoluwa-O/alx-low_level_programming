c335ce587aa8ac2345d458fcfb9d43a470d3796a
Ifeoluwa <ifeonifade@yahoo.com>
2022-09-24 16:11:03 -0700

    try

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,15 +13,15 @@ void print_to_98(int n)
 	{
 		printf("%d, ", n);
 		n++;
-		printf("%d\n", 98);
 	}
+		printf("%d\n", 98);
 	else if (n > 98)
 	{
 		printf("%d, ", n);
 		n--;
-		printf("%d\n", 98);
 	}
-	else
+		printf("%d\n", 98);
+	if (n == 98)
 	{
 		printf("%d\n", 98);
 	}
