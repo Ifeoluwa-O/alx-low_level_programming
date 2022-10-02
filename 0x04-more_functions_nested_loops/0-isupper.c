df0cd38ebd17edde25236cf58f8cb473d3a0f955
Ifeoluwa <ifeonifade@yahoo.com>
2022-10-02 15:21:55 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main- a function that checks for uppercase character
+ * @c: variable
+ * Return: 1 if c is uppercase, and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	for (c = 65; c <= 90; c++)
+	{
+		_putchar("%d", 1);
+		return(1);
+	}
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar("%d", 0);
+		return(0);
+	}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A directory for more nested loop tasks in C
