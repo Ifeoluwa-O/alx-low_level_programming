456debe48d99a6a54bbdfb5b1be3110bd9ac42f8
Ifeoluwa <ifeonifade@yahoo.com>
2022-10-02 15:24:25 -0700

    correct task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main- a function that checks for uppercase character
+ * _isupper - a function that checks for uppercase character
  * @c: variable
  * Return: 1 if c is uppercase, and 0 otherwise
  */
@@ -11,10 +11,10 @@ int _isupper(int c)
 	for (c = 65; c <= 90; c++)
 	{
 		_putchar("%d", 1);
-		return(1);
+		return (1);
 	}
 	for (c = 97; c <= 122; c++)
 	{
 		_putchar("%d", 0);
-		return(0);
+		return (0);
 	}
