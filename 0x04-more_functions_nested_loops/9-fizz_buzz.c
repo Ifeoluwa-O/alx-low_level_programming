18b0a2ba557b400ce921a77508890953b04b330d
Ifeoluwa <ifeonifade@yahoo.com>
2022-10-03 07:23:19 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main- a fizzbuzz program
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 1; t <= 100; t++)
+	{
+		if (t % 3 == 0 && t % 5 != 0)
+			printf("Fizz ");
+		else if (t % 3 != 0 && t % 5 == 0)
+			printf("Buzz ");
+		else if (t % 3 == 0 && t % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", t);
+		return (0);
+	}
+}
